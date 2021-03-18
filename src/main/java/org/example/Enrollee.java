99d41029877a049ee54074e1d6c8aedf22dbb62c
ScottDuke205 <snd1964@gmail.com>
2021-03-18 10:28:48 -0500

    Moved annotations Getter and Setter to class level. Added NoArgsConstructor annotation.

--- src/main/java/org/example/Enrollee.java
+++ src/main/java/org/example/Enrollee.java
@@ -1,27 +1,17 @@
 package org.example;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class Enrollee {
 
-    @Getter
-    @Setter
     private String userId;
-
-    @Getter
-    @Setter
     private String firstName;
-
-    @Getter
-    @Setter
     private String lastName;
-
-    @Getter
-    @Setter
     private int version;
-
-    @Getter
-    @Setter
     private String insuranceCompany;
 }
